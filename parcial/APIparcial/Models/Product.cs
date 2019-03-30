3718f8934fd270bfd8c6f8eb37338de8bc8ccef1
Jose Mario Lami <jmlami99@gmail.com>
2019-03-30 13:01:24 -0400

    Commit modelo con web API

---
+++ parcial/APIparcial/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace APIparcial.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum UniType
+    {
+        Botella,
+        Metro,
+        Litro,
+        Caja
+    }
+
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        [Required]
+        public UniType Unit { get; set; }
+        public StatusType Status { get; set; }
+
+    }
+}

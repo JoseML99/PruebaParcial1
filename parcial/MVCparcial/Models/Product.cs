26800969dc336e8d908ca01d258378258cdd1318
Jose Mario Lami <jmlami99@gmail.com>
2019-03-30 12:33:27 -0400

    Commit con modelo MVC

---
+++ parcial/MVCparcial/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace MVCparcial.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum UniType
+    {
+        Botello,
+        Metro,
+        Litro,
+        Caja
+    }
+
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        [Required]
+        public UniType Unit { get; set; }
+        public StatusType Status { get; set; }
+
+    }
+}
